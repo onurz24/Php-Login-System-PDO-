6455e32703da998576e235a74d5081a7d17c7c9b
onurz24 <87843515+onurz24@users.noreply.github.com>
2023-04-09 14:57:45 +0200

    Update anfrage.js

--- anfrage.js
+++ anfrage.js
@@ -17,3 +17,42 @@ async function postData() {
 }
 
 postData();
+
+
+
+/* ODER MIT FORMULAR */
+
+/*
+<form id="myForm">
+  <label for="username">Username:</label>
+  <input type="text" id="username" name="username">
+  <br>
+  <label for="password">Password:</label>
+  <input type="password" id="password" name="password">
+  <br>
+  <button type="submit">Submit</button>
+</form>
+
+
+async function postData() {
+  const form = document.querySelector('#myForm');
+
+  try {
+    const response = await fetch('https://www.example.com/api', {
+      method: 'POST',
+      body: new FormData(form)
+    });
+
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+postData();
+
+
+
+
+*/
